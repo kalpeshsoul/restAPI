6116918081f8839ef076d23f797b515731107b4a
Kalpesh Patil <llms.mphasis51@sbi.co.in>
2024-10-17 18:19:40 +0530

    ******** added by kp

--- src/main/java/com/spring/demo/FirstprogrammApplication.java
+++ src/main/java/com/spring/demo/FirstprogrammApplication.java
@@ -12,7 +12,9 @@ public class FirstprogrammApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstprogrammApplication.class, args);
 		System.out.println("Hi i am building spring boot application.");
-		System.out.println("GIT--GITHUB--JENKINS");
+		System.out.println("GIT--GITHUB--JENKINS*****************************");
+		
+		
 	}
 
 }
